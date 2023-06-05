2a4f9d3414814a8b29b5be861eeccde6b0f77b91
Fuzail Palnak <fuzailpalnak@gmail.com>
2023-06-05 14:39:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 install_requires = [
     "imgaug == 0.4.0",
-    "numpy == 1.19.1",
-    "image-fragment==0.2.2",
-    "py-oneliner == 0.0.1",
+    "numpy >= 1.19.1",
+    "image-fragment>=0.2.2",
+    "py-oneliner >= 0.0.1",
 ]
 
 setup(
     name="ttAugment",
-    version="0.4.1",
+    version="0.4.2",
     author="Fuzail Palnak",
     author_email="fuzailpalnak@gmail.com",
     description="Test Time Augmentations",
